onap/archive-ccsdk-sli-core

MdsalHelper testing and loggingh
make one log statement quieter and add unit tests for MdsalHelper

Issue-ID: CCSDK-2071
Signed-off-by: Smokowski, Kevin (ks6305) <kevin.smokowski@att.com>
Change-Id: I7af1b2c0dce47c2d5970de4950c951c58fa4fb65

sli/provider/src/main/java/org/onap/ccsdk/sli/core/sli/provider/MdsalHelper.java
@@ -695,7 +695,8 @@ public class MdsalHelper {
                                     builderObj = builderClass.newInstance();
                                     paramObj = toBuilder(props, propNamePfx, builderObj);
                                 } catch (ClassNotFoundException e) {
-                                    LOG.info("Builder class {} not found ", builderName, e);
+                                  LOG.info("Builder class {} not found catching ClassNotFoundException and trying other methods",
+                      builderName);
                                     if (paramValue == null) {
                                         try {
                                             boolean isAccessible = m.isAccessible();

onap/ccsdk-sli

sli/provider/src/main/java/org/onap/ccsdk/sli/core/sli/provider/MdsalHelper.java
@@ -695,7 +695,8 @@ public class MdsalHelper {
                                     builderObj = builderClass.newInstance();
                                     paramObj = toBuilder(props, propNamePfx, builderObj);
                                 } catch (ClassNotFoundException e) {
-                                    LOG.info("Builder class {} not found ", builderName, e);
+                                  LOG.trace("Builder class {} not found catching ClassNotFoundException and trying other methods",
+                      builderName);
                                     if (paramValue == null) {
                                         try {
                                             boolean isAccessible = m.isAccessible();