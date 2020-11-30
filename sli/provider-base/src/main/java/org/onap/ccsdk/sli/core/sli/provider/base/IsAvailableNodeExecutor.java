onap/archive-ccsdk-sli-core

refactor sli-provider
split sli-provider into sli-provider and sli-provider-base

Change-Id: I7b7edb767c04c9a6caf72a9172f49518655e33b7
Issue-ID: CCSDK-778
Signed-off-by: Smokowski, Kevin (ks6305) <kevin.smokowski@att.com>

sli/provider-base/src/main/java/org/onap/ccsdk/sli/core/sli/provider/base/IsAvailableNodeExecutor.java
@@ -1,0 +1,71 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : CCSDK
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights
+ * 						reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.ccsdk.sli.core.sli.provider.base;
+
+import org.onap.ccsdk.sli.core.sli.SvcLogicContext;
+import org.onap.ccsdk.sli.core.sli.SvcLogicException;
+import org.onap.ccsdk.sli.core.sli.SvcLogicNode;
+import org.onap.ccsdk.sli.core.sli.SvcLogicResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IsAvailableNodeExecutor extends SvcLogicNodeExecutor {
+
+    private static final Logger LOG = LoggerFactory.getLogger(IsAvailableNodeExecutor.class);
+
+    @Override
+    public SvcLogicNode execute(SvcLogicServiceBase svc, SvcLogicNode node, SvcLogicContext ctx)
+            throws SvcLogicException {
+
+        String plugin = SvcLogicExpressionResolver.evaluate(node.getAttribute("plugin"), node, ctx);
+        String resourceType = SvcLogicExpressionResolver.evaluate(node.getAttribute("resource"), node, ctx);
+        String key = SvcLogicExpressionResolver.evaluateAsKey(node.getAttribute("key"), node, ctx);
+        String pfx = SvcLogicExpressionResolver.evaluate(node.getAttribute("pfx"), node, ctx);
+
+        String outValue = "failure";
+
+        SvcLogicResource resourcePlugin = getSvcLogicResource(plugin);
+
+        if (resourcePlugin != null) {
+            try {
+                switch (resourcePlugin.isAvailable(resourceType, key, pfx, ctx)) {
+                    case SUCCESS:
+                        outValue = "true";
+                        break;
+                    case NOT_FOUND:
+                        outValue = "false";
+                        break;
+                    case FAILURE:
+                    default:
+                        outValue = "false";
+                }
+            } catch (SvcLogicException e) {
+                LOG.error("Caught exception from resource plugin", e);
+                outValue = "failure";
+            }
+        } else {
+            LOG.warn("Could not find SvcLogicResource object for plugin " + plugin);
+        }
+        return (getNextNode(node, outValue));
+    }
+
+}

sli/provider-base/src/main/java/org/onap/ccsdk/sli/core/sli/provider/base/RecordNodeExecutor.java
@@ -1,0 +1,82 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : CCSDK
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights
+ * 						reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.ccsdk.sli.core.sli.provider.base;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import org.onap.ccsdk.sli.core.sli.SvcLogicContext;
+import org.onap.ccsdk.sli.core.sli.SvcLogicException;
+import org.onap.ccsdk.sli.core.sli.SvcLogicExpression;
+import org.onap.ccsdk.sli.core.sli.SvcLogicNode;
+import org.onap.ccsdk.sli.core.sli.SvcLogicRecorder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RecordNodeExecutor extends SvcLogicNodeExecutor {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RecordNodeExecutor.class);
+
+    @Override
+    public SvcLogicNode execute(SvcLogicServiceBase svc, SvcLogicNode node, SvcLogicContext ctx)
+            throws SvcLogicException {
+
+        String plugin = SvcLogicExpressionResolver.evaluate(node.getAttribute("plugin"), node, ctx);
+        String outValue = "failure";
+
+        if (LOG.isTraceEnabled()) {
+            LOG.trace(node.getNodeType() + " node encountered - looking for recorder class " + plugin);
+        }
+
+        Map<String, String> parmMap = new HashMap<String, String>();
+
+        Set<Map.Entry<String, SvcLogicExpression>> parmSet = node.getParameterSet();
+        boolean hasParms = false;
+
+        for (Iterator<Map.Entry<String, SvcLogicExpression>> iter = parmSet.iterator(); iter.hasNext();) {
+            hasParms = true;
+            Map.Entry<String, SvcLogicExpression> curEnt = iter.next();
+            String curName = curEnt.getKey();
+            SvcLogicExpression curExpr = curEnt.getValue();
+            String curExprValue = SvcLogicExpressionResolver.evaluate(curExpr, node, ctx);
+
+            if (LOG.isTraceEnabled()) {
+                LOG.trace("executeRecordNode : parameter " + curName + " = " + curExpr + " => " + curExprValue);
+            }
+            parmMap.put(curName, curExprValue);
+        }
+
+        SvcLogicRecorder recorder = getSvcLogicRecorder(plugin);
+        if (recorder != null) {
+            try {
+                recorder.record(parmMap);
+            } catch (SvcLogicException e) {
+                LOG.error("Caught exception from recorder plugin", e);
+                outValue = "failure";
+            }
+        } else {
+            LOG.warn("Could not find SvcLogicRecorder object for plugin " + plugin);
+        }
+        return (getNextNode(node, outValue));
+    }
+}

sli/provider-base/src/main/java/org/onap/ccsdk/sli/core/sli/provider/base/ReturnNodeExecutor.java
@@ -1,0 +1,78 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : CCSDK
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights
+ * 						reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.ccsdk.sli.core.sli.provider.base;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.onap.ccsdk.sli.core.sli.SvcLogicContext;
+import org.onap.ccsdk.sli.core.sli.SvcLogicException;
+import org.onap.ccsdk.sli.core.sli.SvcLogicExpression;
+import org.onap.ccsdk.sli.core.sli.SvcLogicNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReturnNodeExecutor extends SvcLogicNodeExecutor {
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(ReturnNodeExecutor.class);
+	
+	@Override
+	public SvcLogicNode execute(SvcLogicServiceBase svc, SvcLogicNode node,
+			SvcLogicContext ctx) throws SvcLogicException {
+
+		String status = SvcLogicExpressionResolver.evaluate(
+				node.getAttribute("status"), node, ctx);
+
+		if (status != null) {
+			if (LOG.isDebugEnabled()) {
+				LOG.debug("Returning status " + status);
+			}
+			ctx.setStatus(status);
+		} else {
+			if (LOG.isWarnEnabled()) {
+				LOG.warn("Return node has no status attribute set");
+			}
+		}
+
+		Set<Map.Entry<String, SvcLogicExpression>> parameterSet = node
+				.getParameterSet();
+
+		for (Iterator<Map.Entry<String, SvcLogicExpression>> iter = parameterSet
+				.iterator(); iter.hasNext();) {
+			Map.Entry<String, SvcLogicExpression> curEnt = iter.next();
+			String curName = curEnt.getKey();
+			String curValue = SvcLogicExpressionResolver.evaluate(
+					curEnt.getValue(), node, ctx);
+
+			if (LOG.isDebugEnabled()) {
+				LOG.debug("Setting context attribute " + curName + " to "
+						+ curValue);
+			}
+			ctx.setAttribute(curName, curValue);
+		}
+		return null;
+	}
+
+
+}

sli/provider-base/src/main/java/org/onap/ccsdk/sli/core/sli/provider/base/WhileNodeExecutor.java
@@ -1,0 +1,75 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : CCSDK
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights
+ * 						reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.ccsdk.sli.core.sli.provider.base;
+
+import org.onap.ccsdk.sli.core.sli.BreakNodeException;
+import org.onap.ccsdk.sli.core.sli.SvcLogicContext;
+import org.onap.ccsdk.sli.core.sli.SvcLogicException;
+import org.onap.ccsdk.sli.core.sli.SvcLogicExpression;
+import org.onap.ccsdk.sli.core.sli.SvcLogicNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WhileNodeExecutor extends SvcLogicNodeExecutor {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WhileNodeExecutor.class);
+
+    @Override
+    public SvcLogicNode execute(SvcLogicServiceBase svc, SvcLogicNode node, SvcLogicContext ctx) throws SvcLogicException {
+
+        String testResult = evaluateNodeTest(node, ctx);
+        SvcLogicExpression silentFailureExpr = node.getAttribute("do");
+        String doWhile = SvcLogicExpressionResolver.evaluate(silentFailureExpr, node, ctx);
+        if ("true".equals(doWhile)) {
+            LOG.debug("While loop will execute once regardless of expression because do is set to true");
+        }
+
+        try {
+            while ("true".equals(testResult) || "true".equals(doWhile)) {
+                if (!"true".equals(doWhile)) {
+                    LOG.debug("Test expression (" + node.getAttribute("test") + ") evaluates to true, executing loop.");
+                }
+                int numOutcomes = node.getNumOutcomes() + 1;
+                for (int i = 0; i < numOutcomes; i++) {
+                    SvcLogicNode nextNode = node.getOutcomeValue("" + (i + 1));
+                    if (nextNode != null) {
+                        while (nextNode != null) {
+                            nextNode = svc.executeNode(nextNode, ctx);
+                        }
+                    } else {
+                        if ("true".equals(doWhile)) {
+                            LOG.debug("Do executed, will only execute again if test expression is true.");
+                            doWhile = "false";
+                        }
+                        testResult = evaluateNodeTest(node, ctx);
+                        LOG.debug("test expression (" + node.getAttribute("test") + ") evaluates to " + testResult);
+                    }
+                }
+            }
+            LOG.debug("testResult was " + testResult + " which is not equal to true, exiting while loop.");
+        } catch (BreakNodeException e) {
+            LOG.debug("WhileNodeExecutor caught break");
+        }
+        return (null);
+    }
+
+}

onap/ccsdk-sli

sli/provider-base/src/main/java/org/onap/ccsdk/sli/core/sli/provider/base/IsAvailableNodeExecutor.java
@@ -1,0 +1,71 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : CCSDK
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights
+ * 						reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.ccsdk.sli.core.sli.provider.base;
+
+import org.onap.ccsdk.sli.core.sli.SvcLogicContext;
+import org.onap.ccsdk.sli.core.sli.SvcLogicException;
+import org.onap.ccsdk.sli.core.sli.SvcLogicNode;
+import org.onap.ccsdk.sli.core.sli.SvcLogicResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IsAvailableNodeExecutor extends AbstractSvcLogicNodeExecutor {
+
+    private static final Logger LOG = LoggerFactory.getLogger(IsAvailableNodeExecutor.class);
+
+    @Override
+    public SvcLogicNode execute(SvcLogicServiceBase svc, SvcLogicNode node, SvcLogicContext ctx)
+            throws SvcLogicException {
+
+        String plugin = SvcLogicExpressionResolver.evaluate(node.getAttribute("plugin"), node, ctx);
+        String resourceType = SvcLogicExpressionResolver.evaluate(node.getAttribute("resource"), node, ctx);
+        String key = SvcLogicExpressionResolver.evaluateAsKey(node.getAttribute("key"), node, ctx);
+        String pfx = SvcLogicExpressionResolver.evaluate(node.getAttribute("pfx"), node, ctx);
+
+        String outValue = "failure";
+
+        SvcLogicResource resourcePlugin = getSvcLogicResource(plugin);
+
+        if (resourcePlugin != null) {
+            try {
+                switch (resourcePlugin.isAvailable(resourceType, key, pfx, ctx)) {
+                    case SUCCESS:
+                        outValue = "true";
+                        break;
+                    case NOT_FOUND:
+                        outValue = "false";
+                        break;
+                    case FAILURE:
+                    default:
+                        outValue = "false";
+                }
+            } catch (SvcLogicException e) {
+                LOG.error("Caught exception from resource plugin", e);
+                outValue = "failure";
+            }
+        } else {
+            LOG.warn("Could not find SvcLogicResource object for plugin " + plugin);
+        }
+        return (getNextNode(node, outValue));
+    }
+
+}

sli/provider-base/src/main/java/org/onap/ccsdk/sli/core/sli/provider/base/RecordNodeExecutor.java
@@ -1,0 +1,82 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : CCSDK
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights
+ * 						reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.ccsdk.sli.core.sli.provider.base;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import org.onap.ccsdk.sli.core.sli.SvcLogicContext;
+import org.onap.ccsdk.sli.core.sli.SvcLogicException;
+import org.onap.ccsdk.sli.core.sli.SvcLogicExpression;
+import org.onap.ccsdk.sli.core.sli.SvcLogicNode;
+import org.onap.ccsdk.sli.core.sli.SvcLogicRecorder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RecordNodeExecutor extends AbstractSvcLogicNodeExecutor {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RecordNodeExecutor.class);
+
+    @Override
+    public SvcLogicNode execute(SvcLogicServiceBase svc, SvcLogicNode node, SvcLogicContext ctx)
+            throws SvcLogicException {
+
+        String plugin = SvcLogicExpressionResolver.evaluate(node.getAttribute("plugin"), node, ctx);
+        String outValue = "failure";
+
+        if (LOG.isTraceEnabled()) {
+            LOG.trace(node.getNodeType() + " node encountered - looking for recorder class " + plugin);
+        }
+
+        Map<String, String> parmMap = new HashMap<String, String>();
+
+        Set<Map.Entry<String, SvcLogicExpression>> parmSet = node.getParameterSet();
+        boolean hasParms = false;
+
+        for (Iterator<Map.Entry<String, SvcLogicExpression>> iter = parmSet.iterator(); iter.hasNext();) {
+            hasParms = true;
+            Map.Entry<String, SvcLogicExpression> curEnt = iter.next();
+            String curName = curEnt.getKey();
+            SvcLogicExpression curExpr = curEnt.getValue();
+            String curExprValue = SvcLogicExpressionResolver.evaluate(curExpr, node, ctx);
+
+            if (LOG.isTraceEnabled()) {
+                LOG.trace("executeRecordNode : parameter " + curName + " = " + curExpr + " => " + curExprValue);
+            }
+            parmMap.put(curName, curExprValue);
+        }
+
+        SvcLogicRecorder recorder = getSvcLogicRecorder(plugin);
+        if (recorder != null) {
+            try {
+                recorder.record(parmMap);
+            } catch (SvcLogicException e) {
+                LOG.error("Caught exception from recorder plugin", e);
+                outValue = "failure";
+            }
+        } else {
+            LOG.warn("Could not find SvcLogicRecorder object for plugin " + plugin);
+        }
+        return (getNextNode(node, outValue));
+    }
+}

sli/provider-base/src/main/java/org/onap/ccsdk/sli/core/sli/provider/base/ReturnNodeExecutor.java
@@ -1,0 +1,78 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : CCSDK
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights
+ * 						reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.ccsdk.sli.core.sli.provider.base;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.onap.ccsdk.sli.core.sli.SvcLogicContext;
+import org.onap.ccsdk.sli.core.sli.SvcLogicException;
+import org.onap.ccsdk.sli.core.sli.SvcLogicExpression;
+import org.onap.ccsdk.sli.core.sli.SvcLogicNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReturnNodeExecutor extends AbstractSvcLogicNodeExecutor {
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(ReturnNodeExecutor.class);
+	
+	@Override
+	public SvcLogicNode execute(SvcLogicServiceBase svc, SvcLogicNode node,
+			SvcLogicContext ctx) throws SvcLogicException {
+
+		String status = SvcLogicExpressionResolver.evaluate(
+				node.getAttribute("status"), node, ctx);
+
+		if (status != null) {
+			if (LOG.isDebugEnabled()) {
+				LOG.debug("Returning status " + status);
+			}
+			ctx.setStatus(status);
+		} else {
+			if (LOG.isWarnEnabled()) {
+				LOG.warn("Return node has no status attribute set");
+			}
+		}
+
+		Set<Map.Entry<String, SvcLogicExpression>> parameterSet = node
+				.getParameterSet();
+
+		for (Iterator<Map.Entry<String, SvcLogicExpression>> iter = parameterSet
+				.iterator(); iter.hasNext();) {
+			Map.Entry<String, SvcLogicExpression> curEnt = iter.next();
+			String curName = curEnt.getKey();
+			String curValue = SvcLogicExpressionResolver.evaluate(
+					curEnt.getValue(), node, ctx);
+
+			if (LOG.isDebugEnabled()) {
+				LOG.debug("Setting context attribute " + curName + " to "
+						+ curValue);
+			}
+			ctx.setAttribute(curName, curValue);
+		}
+		return null;
+	}
+
+
+}

sli/provider-base/src/main/java/org/onap/ccsdk/sli/core/sli/provider/base/WhileNodeExecutor.java
@@ -1,0 +1,75 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : CCSDK
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights
+ * 						reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.ccsdk.sli.core.sli.provider.base;
+
+import org.onap.ccsdk.sli.core.sli.BreakNodeException;
+import org.onap.ccsdk.sli.core.sli.SvcLogicContext;
+import org.onap.ccsdk.sli.core.sli.SvcLogicException;
+import org.onap.ccsdk.sli.core.sli.SvcLogicExpression;
+import org.onap.ccsdk.sli.core.sli.SvcLogicNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WhileNodeExecutor extends AbstractSvcLogicNodeExecutor {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WhileNodeExecutor.class);
+
+    @Override
+    public SvcLogicNode execute(SvcLogicServiceBase svc, SvcLogicNode node, SvcLogicContext ctx) throws SvcLogicException {
+
+        String testResult = evaluateNodeTest(node, ctx);
+        SvcLogicExpression silentFailureExpr = node.getAttribute("do");
+        String doWhile = SvcLogicExpressionResolver.evaluate(silentFailureExpr, node, ctx);
+        if ("true".equals(doWhile)) {
+            LOG.debug("While loop will execute once regardless of expression because do is set to true");
+        }
+
+        try {
+            while ("true".equals(testResult) || "true".equals(doWhile)) {
+                if (!"true".equals(doWhile)) {
+                    LOG.debug("Test expression (" + node.getAttribute("test") + ") evaluates to true, executing loop.");
+                }
+                int numOutcomes = node.getNumOutcomes() + 1;
+                for (int i = 0; i < numOutcomes; i++) {
+                    SvcLogicNode nextNode = node.getOutcomeValue("" + (i + 1));
+                    if (nextNode != null) {
+                        while (nextNode != null) {
+                            nextNode = svc.executeNode(nextNode, ctx);
+                        }
+                    } else {
+                        if ("true".equals(doWhile)) {
+                            LOG.debug("Do executed, will only execute again if test expression is true.");
+                            doWhile = "false";
+                        }
+                        testResult = evaluateNodeTest(node, ctx);
+                        LOG.debug("test expression (" + node.getAttribute("test") + ") evaluates to " + testResult);
+                    }
+                }
+            }
+            LOG.debug("testResult was " + testResult + " which is not equal to true, exiting while loop.");
+        } catch (BreakNodeException e) {
+            LOG.debug("WhileNodeExecutor caught break");
+        }
+        return (null);
+    }
+
+}