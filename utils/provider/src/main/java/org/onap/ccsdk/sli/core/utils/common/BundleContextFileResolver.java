onap/archive-ccsdk-sli-core

Generalization of CCSDK core/utils framework
Changes made:
* Created generalized version of core/utils/dblib as core/utils/common
* Deprecated core/utils/dblib package

Change-Id: I0992c43910278fbe254674d1e39d7e4fcad0a592
Issue-ID: CCSDK-168
Signed-off-by: Rich Tabedzki <richard.tabedzki@att.com>

utils/provider/src/main/java/org/onap/ccsdk/sli/core/utils/common/BundleContextFileResolver.java
@@ -1,0 +1,62 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * openECOMP : SDN-C
+ * ================================================================================
+ * Copyright (C) 2017 - 2018 AT&T Intellectual Property. All rights
+ * 						reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.ccsdk.sli.core.utils.common;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.onap.ccsdk.sli.core.utils.DefaultFileResolver;
+import org.osgi.framework.FrameworkUtil;
+
+import com.google.common.base.Strings;
+
+/**
+ * Resolves properties files from runtime property value <code>SDNC_CONFIG_DIR</code> defined in the osgi properties.
+ */
+public class BundleContextFileResolver extends DefaultFileResolver {
+
+    /**
+     * Key for osgi variable representing the configuration directory
+     */
+	private static final String SDNC_CONFIG_DIR_PROP_KEY = "SDNC_CONFIG_DIR";
+    /**
+     * Default path to look for the configuration directory
+     */
+	private static final Path DEFAULT_DBLIB_PROP_DIR = Paths.get("/opt", "sdnc", "data", "properties");
+
+    public BundleContextFileResolver(final String successMessage, final Class<?> clazz) {
+    	super(successMessage, getPropertiesPath(clazz, SDNC_CONFIG_DIR_PROP_KEY));
+    }
+
+    private static Path getPropertiesPath(Class<?> clazz, String osgiPropertyKey) {
+    	if(FrameworkUtil.getBundle(clazz) == null) {
+    		return DEFAULT_DBLIB_PROP_DIR;
+    	} else {
+        	String path = FrameworkUtil.getBundle(clazz).getBundleContext().getProperty(osgiPropertyKey);
+        	if(Strings.isNullOrEmpty(path)) {
+        		return DEFAULT_DBLIB_PROP_DIR;
+        	}
+        	return Paths.get(path);
+    	}
+
+    }
+}

onap/ccsdk-sli

utils/provider/src/main/java/org/onap/ccsdk/sli/core/utils/common/BundleContextFileResolver.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,32 +18,33 @@
  * ============LICENSE_END=========================================================
  */
 
-package org.onap.ccsdk.sli.core.utils;
+package org.onap.ccsdk.sli.core.utils.common;
 
 import java.io.File;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
+
+import org.onap.ccsdk.sli.core.utils.PropertiesFileResolver;
 import org.osgi.framework.FrameworkUtil;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Strings;
+
 /**
- * Resolves project properties files relative to the directory identified by the JRE property
- * <code>dblib.properties</code>.
+ * Resolves properties files from runtime property value <code>SDNC_CONFIG_DIR</code> defined in the osgi properties.
  */
-public class JREFileResolver implements PropertiesFileResolver {
+public class BundleContextFileResolver implements PropertiesFileResolver {
 
     /**
-     * Key for JRE argument representing the configuration directory
+     * Key for osgi variable representing the configuration directory
      */
+    private static final String SDNC_CONFIG_DIR_PROP_KEY = "SDNC_CONFIG_DIR";
 
     private final String successMessage;
-    private final Class clazz;
+    private final Class<?> clazz;
 
-    public JREFileResolver(final String successMessage, final Class clazz) {
+    public BundleContextFileResolver(final String successMessage, final Class<?> clazz) {
         this.successMessage = successMessage;
         this.clazz = clazz;
     }
@@ -55,17 +56,20 @@ public class JREFileResolver implements PropertiesFileResolver {
      */
     @Override
     public Optional<File> resolveFile(final String filename) {
-        final URL jreArgumentUrl = FrameworkUtil.getBundle(this.clazz)
-                .getResource(filename);
-        try {
-            if (jreArgumentUrl == null) {
+        if(FrameworkUtil.getBundle(clazz) == null) {
+            return Optional.empty();
+        } else {
+            try {
+                final String pathProperty = FrameworkUtil.getBundle(this.clazz).getBundleContext().getProperty(SDNC_CONFIG_DIR_PROP_KEY);
+                if(Strings.isNullOrEmpty(pathProperty)) {
+                    return Optional.empty();
+                }
+                final Path dblibPath = Paths.get(pathProperty);
+                return Optional.of(dblibPath.resolve(filename).toFile());
+            } catch(final Exception e) {
+                LoggerFactory.getLogger(this.getClass()).error("", e);
                 return Optional.empty();
             }
-            final Path dblibPath = Paths.get(jreArgumentUrl.toURI());
-            return Optional.of(dblibPath.resolve(filename).toFile());
-        } catch(final URISyntaxException | FileSystemNotFoundException e) {
-            LoggerFactory.getLogger(this.getClass()).error("", e);
-            return Optional.empty();
         }
     }
 