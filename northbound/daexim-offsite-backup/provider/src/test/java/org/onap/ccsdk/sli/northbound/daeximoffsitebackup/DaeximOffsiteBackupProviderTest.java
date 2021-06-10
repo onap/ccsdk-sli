a4dd5065bafd765e7e13d421597e6fcd58fc73e3
Singal, Kapil (ks220y) <ks220y@att.com>
2021-06-09 23:22:53 -0400

    Upgrading akka cluster dependency
    Updating odlparent versions to 2.1.7-SNAPSHOT
    
    Issue-ID: CCSDK-3333
    Signed-off-by: Singal, Kapil (ks220y) <ks220y@att.com>
    Change-Id: Ia5da682990acbf99a2026393f8dea511ccca4420
    (cherry picked from commit e12652babc98c403fefead2c83395a5439df7dac)

--- northbound/daexim-offsite-backup/provider/src/test/java/org/onap/ccsdk/sli/northbound/daeximoffsitebackup/DaeximOffsiteBackupProviderTest.java
+++ northbound/daexim-offsite-backup/provider/src/test/java/org/onap/ccsdk/sli/northbound/daeximoffsitebackup/DaeximOffsiteBackupProviderTest.java
@@ -33,8 +33,10 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
+import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 import org.junit.Before;
 import org.junit.Test;
@@ -148,8 +150,13 @@ public class DaeximOffsiteBackupProviderTest {
                 return "Some Timestamp";
             }
 
+            @Override
+            public @NonNull Map<Class<? extends Augmentation<RetrieveDataInput>>, Augmentation<RetrieveDataInput>> augmentations() {
+                return null;
+            }
 
         };
+
         try {
             assertNotNull(provider.retrieveData(input));
         }
