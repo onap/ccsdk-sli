f6e22086cb38ac8a0eafc479b305c1edea4953f0
Ganesh <ganesh.c@samsung.com>
2022-04-07 13:02:06 +0530

    Merged CCSDKadaptor unit test cases added
    Signed-off-by: Ganesh <ganesh.c@samsung.com>
    Change-Id: If4b4969c30219c55b6455323348b222d2f6df69f
    Issue-ID: CCSDK-3476

--- adaptors/sql-resource/provider/src/test/java/org/onap/ccsdk/sli/adaptors/resource/sql/SqlResourceProviderTest.java
+++ adaptors/sql-resource/provider/src/test/java/org/onap/ccsdk/sli/adaptors/resource/sql/SqlResourceProviderTest.java
@@ -1,3 +1,25 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : CCSDK
+ * ================================================================================
+ * Copyright (C) 2022 Samsung Electronics
+ * =============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
+ * ============LICENSE_END=========================================================
+ */
+
 package org.onap.ccsdk.sli.adaptors.resource.sql;
 
 import static org.junit.Assert.assertNotNull;
@@ -35,4 +57,21 @@ public class SqlResourceProviderTest {
         assertNotNull(properties);
     }
 
+    @Test
+    public void testReportSuccess() {
+        try{
+            Map<String, String> env = System.getenv();
+            Class<?> cl = env.getClass();
+            Field field = cl.getDeclaredField("m");
+            field.setAccessible(true);
+            Map<String, String> writableEnv = (Map<String, String>) field.get(env);
+            writableEnv.put(SDNC_CONFIG_DIR, "./src/test/resources");
+        } catch (Exception e) {
+            throw new IllegalStateException("Failed to set environment variable", e);
+        }
+
+        provider = new SqlResourcePropertiesProviderImpl();
+        Properties properties = provider.getProperties();
+        assertNotNull(properties);
+    }
 }
