0674a8b0a350eccd19aae5a397a39fd949cc7f0e
vvarvate <vv00489503@techmahindra.com>
2022-11-30 23:38:43 +0530

    Inorder to complete the ANR SON Use case control loop, below changes have been done. 1)After consuming the kafka message from Policy,storing RequestID and   SubRequestID runtime. 2)based on the sli graph execution, success or failure response is sent   back to policy A1-P-RSP topic to fullfill the feedback loop. 3)added support to send response to A1-P-RSP.
      above all was lacking and identified during the integration testing and is been fixed.
    
    Issue-ID: INT-2127
    Signed-off-by: vvarvate <vv00489503@techmahindra.com>
    Change-Id: I2793ca0068a721352d7ba137ec06e8d98b19f532

--- northbound/dmaap-listener/src/main/java/org/onap/ccsdk/sli/northbound/dmaapclient/A1AdapterPolicyDmaapConsumer.java
+++ northbound/dmaap-listener/src/main/java/org/onap/ccsdk/sli/northbound/dmaapclient/A1AdapterPolicyDmaapConsumer.java
@@ -24,7 +24,26 @@ package org.onap.ccsdk.sli.northbound.dmaapclient;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Authenticator;
+import java.net.HttpURLConnection;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+import javax.net.SocketFactory;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.TrustManager;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLSession;
+import org.apache.commons.codec.binary.Base64;
+import org.onap.ccsdk.sli.core.utils.common.AcceptIpAddressHostNameVerifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,6 +55,8 @@ public class A1AdapterPolicyDmaapConsumer extends SdncDmaapConsumerImpl {
     private static final String RPC = "rpc-name";
     private static final String INPUT = "input";
     private static final String PAYLOAD = "Payload";
+    private static final String CORRELATION_ID = "correlation-id";
+    private static final String COMMON_HEADER = "CommonHeader";
 
 
     @Override
@@ -71,6 +92,50 @@ public class A1AdapterPolicyDmaapConsumer extends SdncDmaapConsumerImpl {
             return;
         }
 
+        LOG.info("Payloadnode  :"+payloadNode.textValue()+"\n");
+        JsonNode payload_node_from_string;
+        try {
+         payload_node_from_string = oMapper.readTree(payloadNode.textValue());
+        } catch (Exception e) {
+            throw new InvalidMessageException("Cannot parse A1-ADAPTER-DMAAP json input", e);
+        }
+
+        JsonNode body_PayloadNode= payload_node_from_string.get(INPUT).get(BODY);
+        if(body_PayloadNode == null) {
+            LOG.warn("Missing body under Payload node in A1-ADAPTER-DMAAP message");
+            return;
+        }
+
+        String body_payload_replaced_string=body_PayloadNode.toString().replaceAll("\"","\\\\\"");
+        body_payload_replaced_string="\""+body_payload_replaced_string+"\"";
+        LOG.info("Body Payload  :"+body_payload_replaced_string+"\n");
+
+        JsonNode input_PayloadNode =payload_node_from_string.get(INPUT);
+        if(input_PayloadNode == null) {
+            LOG.warn("Missing body under Payload node in A1-ADAPTER-DMAAP message");
+            return;
+        }
+        ((ObjectNode)input_PayloadNode).put(BODY,body_payload_replaced_string);
+        ((ObjectNode)payload_node_from_string).set("input",input_PayloadNode);
+
+
+
+        JsonNode CommonHeader = input.get(COMMON_HEADER);
+        if(CommonHeader == null) {
+            LOG.info("Missing CommonHeader node.");
+            return;
+        }
+
+
+
+        JsonNode correlation_id = a1AdapterRootNode.get(CORRELATION_ID);
+        if(correlation_id == null) {
+          LOG.info("Missing correlation_id node.");
+          return;
+        }
+        String correlationId= correlation_id.textValue();
+
+
         String rpcMsgbody;
         try {
             	ObjectMapper mapper = new ObjectMapper();
@@ -80,6 +145,9 @@ public class A1AdapterPolicyDmaapConsumer extends SdncDmaapConsumerImpl {
             LOG.error("Unable to parse payload in A1-ADAPTER-DMAAP message", e);
             return;
         }
+        rpcMsgbody= payload_node_from_string.toString().replaceAll("\\\\\\\\\\\\\"","\\\\\"");
+        rpcMsgbody= rpcMsgbody.replaceAll(":\"\\\\\"",":\"");
+        rpcMsgbody= rpcMsgbody.replaceAll("\\\\\"\"","\"");
 
         JsonNode rpcNode = a1AdapterRootNode.get(RPC);
         if(rpcNode == null) {
@@ -87,6 +155,8 @@ public class A1AdapterPolicyDmaapConsumer extends SdncDmaapConsumerImpl {
             return;
         }
         String rpc = rpcNode.textValue();
+       if(rpc.contains("puta1policy"))
+        rpc = "putA1Policy";
         String sdncEndpoint = "A1-ADAPTER-API:" + rpc;
 
         try {
@@ -96,8 +166,81 @@ public class A1AdapterPolicyDmaapConsumer extends SdncDmaapConsumerImpl {
             LOG.info("POST A1-ADAPTER-API Request " + rpcMsgbody);
             if ((odlUrlBase != null) && (odlUrlBase.length() > 0)) {
                 SdncOdlConnection conn = SdncOdlConnection.newInstance(odlUrlBase + "/" + sdncEndpoint, odlUser, odlPassword);
+                String resp=conn.send("POST", "application/json", rpcMsgbody);
 
-                conn.send("POST", "application/json", rpcMsgbody);
+
+                LOG.info("Recieved response code " + resp);
+                int code;
+                String value;
+                if(resp.contains("200")|| resp.contains("201")) {
+                 code=200;
+                 value="SUCCESS";
+                }else {
+                 code=400;
+                 value="FAILURE";
+                }
+
+             ObjectMapper mapper = new ObjectMapper();
+             ObjectNode a1p_rsp_node = mapper.createObjectNode();
+
+             a1p_rsp_node.put("version", "1.0");
+             a1p_rsp_node.put("rpc-name", rpc );
+             a1p_rsp_node.put("correlation-id", correlationId);
+             a1p_rsp_node.put("type", "response");
+
+             ObjectNode a1p_rsp_body_Status = mapper.createObjectNode();
+             a1p_rsp_body_Status.put("Code", code);
+             a1p_rsp_body_Status.put("Value", value);
+
+             ObjectNode a1p_rsp_body_output = mapper.createObjectNode();
+             a1p_rsp_body_output.set("CommonHeader", CommonHeader);
+             a1p_rsp_body_output.set("Status", a1p_rsp_body_Status);
+             a1p_rsp_body_output.put("Payload", "");
+
+             ObjectNode a1p_rsp_body = mapper.createObjectNode();
+             a1p_rsp_body.set("output", a1p_rsp_body_output);
+             a1p_rsp_node.set("body", a1p_rsp_body);
+
+             String jsonString = mapper.writer().writeValueAsString(a1p_rsp_node);
+             System.out.println(jsonString);
+
+             URL a1_p_Rspurl = new URL("http://message-router:3904/events/A1-P-RSP");
+             HttpURLConnection httpConn = (HttpURLConnection) a1_p_Rspurl.openConnection();
+             httpConn.setRequestMethod("POST");
+                httpConn.setRequestProperty("Content-Type", "application/json");
+                httpConn.setRequestProperty("Accept", "application/json");
+                httpConn.setDoInput(true);
+                httpConn.setDoOutput(true);
+                httpConn.setUseCaches(false);
+                if (httpConn instanceof HttpsURLConnection) {
+
+                 HostnameVerifier hostnameVerifier = new AcceptIpAddressHostNameVerifier();
+                    ((HttpsURLConnection) httpConn).setHostnameVerifier(hostnameVerifier);
+                }
+                // Write message
+                httpConn.setRequestProperty("Content-Length", Integer.toString(jsonString.length()));
+                DataOutputStream outStr = new DataOutputStream(httpConn.getOutputStream());
+                outStr.write(jsonString.getBytes());
+                outStr.close();
+                // Read response
+                BufferedReader respRdr;
+                LOG.info("A1-P_RSP Response: " + httpConn.getResponseCode() + " " + httpConn.getResponseMessage());
+                if (httpConn.getResponseCode() < 300) {
+                    respRdr = new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
+                } else {
+                    respRdr = new BufferedReader(new InputStreamReader(httpConn.getErrorStream()));
+                }
+                StringBuilder respBuff = new StringBuilder();
+                String respLn;
+                while ((respLn = respRdr.readLine()) != null) {
+                    respBuff.append(respLn).append("\n");
+                }
+                respRdr.close();
+                String respString = respBuff.toString();
+                LOG.info(String.format("A1-P_RSP Response body :%n%s", respString));
+
+
+
             } else {
                 LOG.warn("Unable to POST A1-ADAPTER-API message. SDNC URL not available. body:\n" + rpcMsgbody);
             }
