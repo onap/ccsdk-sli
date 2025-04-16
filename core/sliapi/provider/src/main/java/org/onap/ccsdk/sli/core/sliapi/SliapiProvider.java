b70e9f3bf3f4ad5666aaf825023656747944ccf1
Dan Timoney <dtimoney@att.com>
2025-04-16 13:23:53 -0400

    Upgrade to OpenDaylight Scandium
    Upgrade ccsdk/sli to OpenDaylight Scandium release. Note: this
    is a major upgrade, with significant breaking changes particularly
    in MDSAL and Yangtools.  Also, it requires Java version 21 and
    maven version 4.9.5 or greater.
    
    Note: major changes are required to one plugin - restconf-client
    due to changes in Yangtools in OpenDayliight Calcium.  For now,
    we have disabled this plugin so the rest of the port can proceed.  A
    later review will restore this plugin with the necessary changes.
    
    Issue-ID: CCSDK-4115
    Change-Id: I2ec78c0b2eae294a375a01219e8b5c8b0ffa9fce
    Signed-off-by: Dan Timoney <dtimoney@att.com>

--- core/sliapi/provider/src/main/java/org/onap/ccsdk/sli/core/sliapi/SliapiProvider.java
+++ core/sliapi/provider/src/main/java/org/onap/ccsdk/sli/core/sliapi/SliapiProvider.java
@@ -21,6 +21,7 @@
 
 package org.onap.ccsdk.sli.core.sliapi;
 
+import static java.util.Objects.requireNonNull;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import java.io.BufferedReader;
@@ -31,22 +32,31 @@ import java.util.Enumeration;
 import java.util.LinkedList;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import jakarta.annotation.PreDestroy;
+
 import org.onap.ccsdk.sli.core.sli.provider.SvcLogicService;
-import org.opendaylight.mdsal.binding.api.NotificationPublishService;
+import org.opendaylight.mdsal.binding.api.DataBroker;
 import org.opendaylight.mdsal.binding.api.RpcProviderService;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.mdsal.common.api.OptimisticLockFailedException;
 import org.opendaylight.mdsal.dom.api.DOMDataBroker;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeWriteTransaction;
+import org.opendaylight.mdsal.eos.binding.api.EntityOwnershipService;
+import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.ExecuteGraph;
 import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.ExecuteGraphInput;
 import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.ExecuteGraphInput.Mode;
 import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.ExecuteGraphInputBuilder;
 import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.ExecuteGraphOutput;
 import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.ExecuteGraphOutputBuilder;
+import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.Healthcheck;
 import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.HealthcheckInput;
 import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.HealthcheckOutput;
 import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.HealthcheckOutputBuilder;
-import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.SLIAPIService;
+import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.TestResults;
+import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.Vlbcheck;
 import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.VlbcheckInput;
 import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.VlbcheckOutput;
 import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.VlbcheckOutputBuilder;
@@ -54,6 +64,8 @@ import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.exe
 import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.test.results.TestResult;
 import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.test.results.TestResultBuilder;
 import org.opendaylight.yangtools.concepts.ObjectRegistration;
+import org.opendaylight.yangtools.concepts.Registration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.common.*;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
@@ -62,12 +74,14 @@ import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeWithV
 import org.opendaylight.yangtools.yang.data.api.schema.LeafSetEntryNode;
 import org.opendaylight.yangtools.yang.data.api.schema.LeafSetNode;
 import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
-import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
-import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableLeafSetEntryNodeBuilder;
-import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableLeafSetNodeBuilder;
+import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceReference;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -105,19 +119,19 @@ import org.slf4j.LoggerFactory;
  *
  * </pre>
  */
-public class SliapiProvider implements AutoCloseable, SLIAPIService {
+@Singleton
+@Component(service = ExecuteGraph.class, immediate = true)
+public class SliapiProvider implements AutoCloseable, ExecuteGraph {
 
 	private static final Logger LOG = LoggerFactory.getLogger(SliapiProvider.class);
-	private static final String appName = "slitester";
 
-	//protected DataBroker dataBroker;
-	protected DOMDataBroker domDataBroker;
-	protected NotificationPublishService notificationService;
-	protected RpcProviderService rpcRegistry;
+	private final DataBroker dataProvider;
+	private final DOMDataBroker dataBroker;
+	private final EntityOwnershipService ownershipService;
 
 	private SvcLogicService svcLogic;
 
-	protected ObjectRegistration<SLIAPIService> rpcRegistration;
+	private final Registration rpcRegistration;
 
 	private static String SLIAPI_NAMESPACE = "org:onap:ccsdk:sli:core:sliapi";
 	private static String SLIAPI_REVISION = "2016-11-10";
@@ -128,7 +142,10 @@ public class SliapiProvider implements AutoCloseable, SLIAPIService {
 	private static QName TEST_RESULT_QNAME = null;
 	private static QName TEST_ID_QNAME = null;
 	private static QName RESULTS_QNAME = null;
-	private static final String NON_NULL = "non-null";
+
+	private static final String appName = "SliapiProvider";
+
+	
 
 	static {
 
@@ -138,37 +155,41 @@ public class SliapiProvider implements AutoCloseable, SLIAPIService {
 		RESULTS_QNAME = QName.create(TEST_RESULT_QNAME, "results");
 	}
 
-	public SliapiProvider(DOMDataBroker dataBroker, NotificationPublishService notificationPublishService,
-	        RpcProviderService rpcProviderRegistry) {
-		this(dataBroker, notificationPublishService, rpcProviderRegistry, findSvcLogicService());
+	@Inject
+	@Activate
+	public SliapiProvider(@Reference final DataBroker dataProvider, 
+	                      @Reference final EntityOwnershipService ownershipService,
+	                      @Reference final DOMDataBroker domDataBroker,
+	                      @Reference final RpcProviderService rpcProviderRegistry) {
+		this(dataProvider, ownershipService, domDataBroker, rpcProviderRegistry, findSvcLogicService());
 	}
 
-	public SliapiProvider(DOMDataBroker dataBroker, NotificationPublishService notificationPublishService,
-	        RpcProviderService rpcProviderRegistry, SvcLogicService svcLogic) {
-		this.LOG.info("Creating provider for " + appName);
-		//this.dataBroker = dataBroker;
-		this.notificationService = notificationPublishService;
-		this.rpcRegistry = rpcProviderRegistry;
+	public SliapiProvider(final DataBroker dataProvider, 
+	                      final EntityOwnershipService ownershipService,
+	                      final DOMDataBroker domDataBroker,
+						  final RpcProviderService rpcProviderRegistry,
+						  SvcLogicService svcLogic) {
+		LOG.info("Creating provider for " + appName);
+		this.dataProvider = dataProvider;
+		this.ownershipService = ownershipService;
 		this.svcLogic = svcLogic;
-		this.domDataBroker = dataBroker;
-		initialize();
-	}
+		this.dataBroker = requireNonNull(domDataBroker);
 
-	public void initialize() {
-		LOG.info("Initializing provider for " + appName);
-		// initialization code goes here.
-		rpcRegistration = rpcRegistry.registerRpcImplementation(SLIAPIService.class, this);
+		rpcRegistration = rpcProviderRegistry.registerRpcImplementations(
+			this,
+			(Healthcheck) this::healthcheck,
+			(Vlbcheck) this::vlbcheck);
 
 		sdncStatusFile = System.getenv(SDNC_STATUS_FILE);
 		LOG.info("SDNC STATUS FILE = " + sdncStatusFile);
 		LOG.info("Initialization complete for " + appName);
 	}
 
-	protected void initializeChild() {
-		// Override if you have custom initialization intelligence
-	}
+
 
 	@Override
+	@PreDestroy
+	@Deactivate
 	public void close() throws Exception {
 		LOG.info("Closing provider for " + appName);
 		// closing code goes here
@@ -177,30 +198,8 @@ public class SliapiProvider implements AutoCloseable, SLIAPIService {
 		LOG.info("Successfully closed provider for " + appName);
 	}
 
-	public void setDataBroker(DOMDataBroker dataBroker) {
-		domDataBroker = dataBroker;
-
-		if (LOG.isDebugEnabled()) {
-			LOG.debug("DataBroker set to " + (dataBroker == null ? "null" : NON_NULL) + ".");
-		}
-	}
-
-	public void setNotificationService(NotificationPublishService notificationService) {
-		this.notificationService = notificationService;
-		if (LOG.isDebugEnabled()) {
-			LOG.debug("Notification Service set to " + (notificationService == null ? "null" : NON_NULL) + ".");
-		}
-	}
-
-	public void setRpcRegistry(RpcProviderService rpcRegistry) {
-		this.rpcRegistry = rpcRegistry;
-		if (LOG.isDebugEnabled()) {
-			LOG.debug("RpcRegistry set to " + (rpcRegistry == null ? "null" : NON_NULL) + ".");
-		}
-	}
-
 	@Override
-	public ListenableFuture<RpcResult<ExecuteGraphOutput>> executeGraph(ExecuteGraphInput input) {
+	public ListenableFuture<RpcResult<ExecuteGraphOutput>> invoke(ExecuteGraphInput input) {
 		RpcResult<ExecuteGraphOutput> rpcResult = null;
 
 		SvcLogicService svcLogic = getSvcLogicService();
@@ -294,7 +293,7 @@ public class SliapiProvider implements AutoCloseable, SLIAPIService {
 				argList = null;
 			}
 
-			Properties respProps = svcLogic.execute(calledModule, calledRpc, null, modeStr, parms, domDataBroker);
+			Properties respProps = svcLogic.execute(calledModule, calledRpc, null, modeStr, parms);
 
 			StringBuilder sb = new StringBuilder("{");
 
@@ -372,7 +371,6 @@ public class SliapiProvider implements AutoCloseable, SLIAPIService {
 		return (svcLogic);
 	}
 
-	@Override
 	public ListenableFuture<RpcResult<HealthcheckOutput>> healthcheck(HealthcheckInput healthcheckInput) {
 
 		RpcResult<HealthcheckOutput> rpcResult = null;
@@ -443,7 +441,6 @@ public class SliapiProvider implements AutoCloseable, SLIAPIService {
 		return (Futures.immediateFuture(rpcResult));
 	}
 
-	@Override
     public ListenableFuture<RpcResult<VlbcheckOutput>> vlbcheck(VlbcheckInput vlbInput) {
 
 		RpcResult<VlbcheckOutput> rpcResult = null;
@@ -547,8 +544,8 @@ public class SliapiProvider implements AutoCloseable, SLIAPIService {
 
 	private void DomSaveTestResult(final TestResult entry, boolean merge, LogicalDatastoreType storeType) {
 
-		if (domDataBroker == null) {
-			LOG.error("domDataBroker unset - cannot save test result using DOMDataBroker");
+		if (dataBroker == null) {
+			LOG.error("dataBroker unset - cannot save test result using DataBroker");
 			return;
 		}
 
@@ -568,12 +565,12 @@ public class SliapiProvider implements AutoCloseable, SLIAPIService {
 		YangInstanceIdentifier testResultsPid = YangInstanceIdentifier.builder().node(TEST_RESULTS_QNAME)
 				.node(QName.create(TEST_RESULTS_QNAME, "test-result")).build();
 		YangInstanceIdentifier testResultPid = testResultsPid
-				.node(NodeIdentifierWithPredicates.of(TEST_RESULT_QNAME, resultNode.getIdentifier().asMap()));
+				.node(NodeIdentifierWithPredicates.of(TEST_RESULT_QNAME, resultNode.name().asMap()));
 
 		int tries = 2;
 		while (true) {
 			try {
-				DOMDataTreeWriteTransaction wtx = domDataBroker.newWriteOnlyTransaction();
+				DOMDataTreeWriteTransaction wtx = dataBroker.newWriteOnlyTransaction();
 				if (merge) {
 					LOG.info("Merging test identifier " + entry.getTestIdentifier());
 					wtx.merge(storeType, testResultPid, resultNode);
@@ -635,6 +632,7 @@ public class SliapiProvider implements AutoCloseable, SLIAPIService {
 //		}
 //	}
 
+
 	private MapEntryNode toMapEntryNode(TestResult testResult) {
 
 		YangInstanceIdentifier testResultId = YangInstanceIdentifier.builder().node(TEST_RESULTS_QNAME)
@@ -643,18 +641,15 @@ public class SliapiProvider implements AutoCloseable, SLIAPIService {
 		// Construct results list
 		LinkedList<LeafSetEntryNode<Object>> entryList = new LinkedList<>();
 		for (String result : testResult.getResults()) {
-			LeafSetEntryNode<Object> leafSetEntryNode = ImmutableLeafSetEntryNodeBuilder.create()
-					.withNodeIdentifier(new NodeWithValue(RESULTS_QNAME, result)).withValue(result).build();
-			entryList.add(leafSetEntryNode);
+			LeafSetEntryNode<Object> leafSetEntryNode = ImmutableNodes.leafSetEntry(RESULTS_QNAME, result);
 		}
 		// Construct results LeafSetNode
-		LeafSetNode<?> resultsNode = ImmutableLeafSetNodeBuilder.create()
-				.withNodeIdentifier(new NodeIdentifier(RESULTS_QNAME)).withValue(entryList).build();
+		LeafSetNode<?> resultsNode =  ImmutableNodes.newUserLeafSetBuilder()
+                               .withNodeIdentifier(new NodeIdentifier(RESULTS_QNAME)).withValue(entryList).build();
 
 		// Construct test result ContainerNode with 2 children - test-identifier leaf
 		// and results leaf-set
-		MapEntryNode testResultNode = ImmutableNodes.mapEntryBuilder()
-
+		MapEntryNode testResultNode = ImmutableNodes.newMapEntryBuilder()
 				.withNodeIdentifier(NodeIdentifierWithPredicates.of(TEST_RESULT_QNAME, TEST_ID_QNAME,
 						testResult.getTestIdentifier()))
 				.withChild(ImmutableNodes.leafNode(TEST_ID_QNAME, testResult.getTestIdentifier()))
